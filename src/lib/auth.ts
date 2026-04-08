45aa938fdb07605db3e22ba1a00f5c1093e2b459
khokan <khokon@gmail.com>
2026-04-08 15:34:00 +0600

    better auth updated

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,43 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { oAuthProxy } from "better-auth/plugins";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+
+    baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: [process.env.FRONTEND_URL!],
+
+    emailAndPassword: {
+      enabled: true,
+    },
+
+    advanced: {
+    cookies: {
+      session_token: {
+        name: "session_token", // Force this exact name
+        attributes: {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+          partitioned: true,
+        },
+      },
+      state: {
+        name: "session_token", // Force this exact name
+        attributes: {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+          partitioned: true,
+        },
+      },
+    },
+  },
+
+     plugins: [oAuthProxy()],
 });
