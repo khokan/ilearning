f0e4ee6e8b99b8900ab8bc828474425081b56da4
khokan <khokon@gmail.com>
2026-04-08 10:06:03 +0600

    prisma added

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import { prisma } from "./lib/prisma";
 
 const app = express();
-const port = 5000; // The port your express server will be running on.
+const port = process.env.PORT || 5000; // The port your express server will be running on.
 
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const bootstrap = async () => {
+    try {
+       await prisma.$connect();
+        app.listen(port, () => {
+            console.log(`Server is running on ${port}`);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        await prisma.$disconnect();
+    }
+}
 
+bootstrap();  
 export default app;
